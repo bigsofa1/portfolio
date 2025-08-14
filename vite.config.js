cb30c602e49ffd3e8254fa89a34cd62f7bd9220f
Thomas V <thomas.vinette3@gmail.com>
2025-08-14 11:59:19 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // If using a project page, set base to "/<REPO>/".
+  // If you will use a custom domain, keep base as "/".
+  base: '/portfolio/',
 })
